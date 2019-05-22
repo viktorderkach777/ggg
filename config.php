892ff51e6f5efc41377864f92c9c9eab66b06eb9
Anonymous <user@rivne.itstep.org>
2019-05-22 20:57:02 +0300

    add table

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_NAME','base');
+define('DB_USER','table2_user');
+define('DB_PASSWORD','FQpjBhQs1g33eBuS');
+define('DB_HOST','localhost');
+define('DB_CHARSET','');//utf8
+?>
--- index.php
+++ index.php
@@ -1,8 +1,25 @@
+
+<?php include_once('connection_database.php'); ?>
+
 <?php include_once("header.php") ?>
 
-    <h1>Home Page</h1>
-    <?php
-        echo "------Hello-----";
-    ?>
+<table class="table">
+<?php
+//$sss = 11;
+$res = $connect->query("SELECT Id,Name,Description FROM tbl_roles");
+$num = 1;
+while ($row = $res->fetch_assoc()) {
+echo "
+<tr>
+<td>{$num}</td>
+<td>" . $row['Name'] . "</td>
+<td>{$row['Description']}</td>
+</tr>
+";
+$num++;
+}
+?>
+</table>
+
 
 <?php include_once("footer.php") ?>
